c8dc9fb8da709acc4dacd0afed25266e7f478e63
leonardo gonzalez <realleo2009@hotmail.com>
2022-08-02 02:30:17 -0300

    Flutter: Navegação entre Páginas, Formulários e Tabs

--- lib/pages/add_titulo_page.dart
+++ lib/pages/add_titulo_page.dart
@@ -20,51 +20,76 @@ class _AddTituloPageState extends State<AddTituloPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Adicionar Título'),
+      appBar: AppBar(
+        title: const Text('Adicionar Título'),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(24),
+              child: TextFormField(
+                controller: _ano,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Ano',
+                ),
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Informe o ano do título';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+              child: TextFormField(
+                controller: _campeonato,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Campeonato',
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Informe qual é o campeonato';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Expanded(
+              child: Container(
+                alignment: Alignment.bottomCenter,
+                margin: const EdgeInsets.all(24),
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      widget.onSave!(
+                          Titulo(ano: _ano.text, campeonato: _campeonato.text));
+                    }
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(Icons.check),
+                      Padding(
+                        padding: EdgeInsets.all(16),
+                        child: Text(
+                          'Salvar',
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
-        body: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(24),
-                child: TextFormField(
-                  controller: _ano,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Ano',
-                  ),
-                  keyboardType: TextInputType.number,
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Informe o ano do título';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 24,vertical: 12),
-                child: TextFormField(
-                  controller: _campeonato,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Ano',
-                  ),
-
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Informe qual é o campeonato';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-            ],
-          ),
-        ));
+      ),
+    );
   }
 }
