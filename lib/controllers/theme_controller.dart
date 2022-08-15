11b489993663597a9c8bb53f6e0bdbe2bbce0424
Leonardo Gonzalez dos Santos <59115521+leozero3@users.noreply.github.com>
2022-08-15 02:08:37 -0300

    Dark Mode no Flutter e Banco de Dados com Hive e SQLite 40:00

--- lib/controllers/theme_controller.dart
+++ lib/controllers/theme_controller.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeController extends GetxController {
-  SharedPreferences? prefs;
+  // SharedPreferences? prefs;
 
   var isDark = false.obs;
   Map<String, ThemeMode> themeModes = {
@@ -15,8 +19,13 @@ class ThemeController extends GetxController {
 
   //lê o SharedPreferences, carrega o tema, chama a funcao setMode
   loadThemeMode() async {
-    prefs = await SharedPreferences.getInstance();
-    String themeText = prefs!.getString('theme') ?? 'light';
+    // prefs = await SharedPreferences.getInstance();
+    // String themeText = prefs!.getString('theme') ?? 'light';
+
+    Directory dir = await getApplicationDocumentsDirectory();
+    var box = await Hive.openBox('preferencias',path: dir.path);
+    String themeText = box.get('theme') ?? 'light';
+    
     isDark.value = themeText == 'dark' ? true : false;
     setMode(themeText);
   }
@@ -25,8 +34,11 @@ class ThemeController extends GetxController {
   Future setMode(String themeText) async {
     ThemeMode? themeMode = themeModes[themeText];
     Get.changeThemeMode(themeMode!);
-    prefs = await SharedPreferences.getInstance();
-    await prefs!.setString('theme', themeText);
+    var box = await Hive.openBox('preferencias');
+    await box.put('theme', themeText);
+
+    // prefs = await SharedPreferences.getInstance();
+    // await prefs!.setString('theme', themeText);
   }
 
   changeTheme() {
