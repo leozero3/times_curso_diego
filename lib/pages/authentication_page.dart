62533ac8bc1665e235fc74fce4004f73665d3f83
leo <realleo2009@hotmail.com>
2022-08-22 23:47:24 -0300

    Como usar o Firebase no Flutter para Autenticação e Banco de Dados 15:00

--- lib/pages/authentication_page.dart
+++ lib/pages/authentication_page.dart
@@ -6,6 +6,37 @@ class AuthenticationPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        toolbarHeight: 80,
+        title: Text(''),
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: Text('dat'),
+            style: ButtonStyle(
+              foregroundColor: MaterialStateProperty.all<Color>(Colors.white70),
+            ),
+          ),
+        ],
+      ),
+      body: Form(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(24),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'Email'),
+                keyboardType: TextInputType.emailAddress,
+                //validator: (){}
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
